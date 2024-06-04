0bd24a67bb6ba4d8ce138c565107405bdce7c96b
jnskkmhr <junjun.k2012@gmail.com>
2024-06-04 15:37:08 +0900

    update readme

--- src/stereo-inertial/stereo-inertial.cpp
+++ src/stereo-inertial/stereo-inertial.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv)
 {
-    if(argc < 6)
+    if(argc < 5)
     {
         std::cerr << "\nUsage: ros2 run orbslam stereo path_to_vocabulary path_to_settings do_rectify do_equalize visualize" << std::endl;
         rclcpp::shutdown();
@@ -22,8 +22,8 @@ int main(int argc, char **argv)
     // malloc error using new.. try shared ptr
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
 
-    // bool visualization = true;
-    ORB_SLAM3::System pSLAM(argv[1], argv[2], ORB_SLAM3::System::IMU_STEREO, argv[5]);
+    bool visualization = false;
+    ORB_SLAM3::System pSLAM(argv[1], argv[2], ORB_SLAM3::System::IMU_STEREO, visualization);
 
     auto node = std::make_shared<StereoInertialNode>(&pSLAM, argv[2], argv[3], argv[4]);
     std::cout << "============================" << std::endl;
--- src/stereo/stereo.cpp
+++ src/stereo/stereo.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv)
 {
-    if(argc < 5)
+    if(argc < 4)
     {
         std::cerr << "\nUsage: ros2 run orbslam stereo path_to_vocabulary path_to_settings do_rectify visualization" << std::endl;
         return 1;
@@ -21,8 +21,8 @@ int main(int argc, char **argv)
     // malloc error using new.. try shared ptr
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
 
-    // bool visualization = true;
-    ORB_SLAM3::System pSLAM(argv[1], argv[2], ORB_SLAM3::System::STEREO, argv[4]);
+    bool visualization = false;
+    ORB_SLAM3::System pSLAM(argv[1], argv[2], ORB_SLAM3::System::STEREO, visualization);
 
     auto node = std::make_shared<StereoSlamNode>(&pSLAM, argv[2], argv[3]);
     std::cout << "============================ " << std::endl;
